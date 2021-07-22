4c9109c8e00069259c382d40fa634e544aaa0c22
flor fuentes <fuentesmflorm@gmail.com>
2021-07-22 15:06:02 -0500

    travis config

---
+++ src/__test__/components/Header.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import ProviderMock from '../../__mocks__/ProviderMock';
+import Header from '../../components/Header';
+import { expect } from '@jest/globals';
+import {create} from 'react-test-renderer';
+
+describe('<Header/>', () => {
+    test('Render del componente Header', () => {
+        const header = shallow(
+            <ProviderMock>
+                <Header></Header>
+            </ProviderMock>
+        );
+        expect(header.length).toEqual(1);
+    });
+
+    test('Render del titulo', () => {
+        const header = mount(
+            <ProviderMock>
+                <Header></Header>
+            </ProviderMock>
+        );
+        expect(header.find('.Header-title').text()).toEqual('Platzi Store');
+    });
+});
+
+describe('Header Snapshot', () => {
+    test('Comprobar el Snapshot de Header', () => {
+        const header = create(
+            <ProviderMock>
+                <Header></Header>
+            </ProviderMock>
+        );
+        expect(header.toJSON()).toMatchSnapshot();
+    });
+});
